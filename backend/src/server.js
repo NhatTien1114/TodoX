e4492219ad9fbe4e5b9c9ab8f2aa3e5eec3076d2
unknown <tongnguyennhattien140805@gmail.com>
2025-11-05 09:29:45 +0700

    Chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,32 @@ import tasksRoutes from './routes/tasksRouters.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from "path"
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
+
+if (proccess.env.NODE_ENV !== "production") {
+    app.use(cors({ origin: 'http://localhost:5173' }));
+}
 
 app.use('/api/tasks', tasksRoutes);
+
+if (proccess.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
+
 
 connectDB().then(() => {
     app.listen(PORT, () => {
